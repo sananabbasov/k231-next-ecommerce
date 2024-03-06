8ba33abe89702b7565ec2fbdcfce86aeb727b5ff
sananabbasov <sanan_abbasov@list.ru>
2024-03-06 18:01:03 +0400

    initial

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from './layout'
+
+function Page() {
+  return (
+    <div>Salam</div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/product/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Detail</div>
+  )
+}
+
+export default page
